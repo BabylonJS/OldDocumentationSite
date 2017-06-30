65160a2c6e66313e99bc97acc9fecc3f7a367456
ameuleman <andreasmeu@gmail.com>
2017-06-30 16:26:47 +0200

    Enables to open the right page using grunt serve

--- scripts/loadIndexes.js
+++ scripts/loadIndexes.js
@@ -15,32 +15,41 @@ var fulltextsearch = require('full-text-search');
 
 module.exports = function(searchDir){
 
-    var dir = fs.readdirSync(searchDir);
-    var uniqNames = [];
-    var searchIndexes = [];
+    // The following code throws an exception when using a local server
+    try{
+        var dir = fs.readdirSync(searchDir);
+        var uniqNames = [];
+        var searchIndexes = [];
 
-    dir.map(function(file){
-        var newName = file.substr(0, file.indexOf('.json'));
+        dir.map(function(file){
+            var newName = file.substr(0, file.indexOf('.json'));
 
-        if(uniqNames.indexOf(newName) == -1){
-            uniqNames.push(newName);
+            if(uniqNames.indexOf(newName) == -1){
+                uniqNames.push(newName);
+            }
+        });
+
+        for (var i = 0; i < uniqNames.length; i++) {
+            //var file =
+            var indexObject = JSON.parse(fileSplitter.readSync(path.join(searchDir, uniqNames[i] +'.json')));
+
+            var index = new fulltextsearch(indexObject.config);
+
+            index.indexes = indexObject.indexes;
+            index.data = indexObject.data;
+            index.data_ptr = indexObject.data_ptr;
+            index.free_slots = indexObject.free_slots;
+            index.single_data_counter = indexObject.single_data_counter;
+
+            searchIndexes.push(index);
         }
-    });
 
-    for (var i = 0; i < uniqNames.length; i++) {
-        //var file =
-        var indexObject = JSON.parse(fileSplitter.readSync(path.join(searchDir, uniqNames[i] +'.json')));
-
-        var index = new fulltextsearch(indexObject.config);
-
-        index.indexes = indexObject.indexes;
-        index.data = indexObject.data;
-        index.data_ptr = indexObject.data_ptr;
-        index.free_slots = indexObject.free_slots;
-        index.single_data_counter = indexObject.single_data_counter;
-
-        searchIndexes.push(index);
+        return searchIndexes;
+    }
+    // Return an empty array for local debug
+    catch(e){
+        console.log(e);   
+        return [];
     }
 
-    return searchIndexes;
 };
