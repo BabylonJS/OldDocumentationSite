bd75bf7e94083de49fe62ca317cb7a672b8d44ac
Bloadrick <aurelien.feret@viacesi.fr>
2018-04-26 17:21:23 +0200

    Example tab

--- scripts/compile-html/compile-html-examples.js
+++ scripts/compile-html/compile-html-examples.js
@@ -11,8 +11,8 @@ var pug = require('pug'),
     path = require('path'),
     appRoot = require('app-root-path').path,
     logger = require(path.join(appRoot, 'config/logger'));
-
-var EXAMPLES_PATH = path.join(appRoot, 'examples/list.json'); 
+ 
+var EXAMPLES_PATH_JSON = path.join(appRoot, 'examples/list.json'); 
 
 /*************************************************************************
  *                                 SCRIPT                                *
@@ -28,41 +28,52 @@ function checkDirectorySync(directory) {
 
 module.exports = function (done) {
     checkDirectorySync("public/html/");
-    let pugContent = `
+    var content = "";
+    let pugContent = 
+`
 include includes/header.pug
 include includes/banner.pug
-#wrapper
-    .whats-new-banner
-        h1 Examples
-    .horizontal-separator
-    .examples-content
-        div(style="width:200px; display:inline-block")
-            .examplesContent
-                input(type='text', name='filter', placeholder='Filter examples...')
-                button
-                    i.fa.fa-search
+#wrapper(style="width:100%;height:90%;margin-top:2px")
+    div(id='examplePage',style="width:400px;display:inline-block;float:left;")
+        h1(style="text-align: center;") Examples
+        .horizontal-separator
+        input(id='searchBar',type='text', name='filter', placeholder='Filter examples...',style="margin-left: 15px")
+        button(onclick="Search()")
+            i.fa.fa-search
 `;
     
-    fs.readFile(EXAMPLES_PATH, function (err, listJson) { 
+    fs.readFile(EXAMPLES_PATH_JSON, function (err, listJson) { 
         var list = JSON.parse(listJson);
+        var elemCountID = 0;
         list.examples.forEach(category => {
-            pugContent += `                ul ` + category.title + "\n";
+            pugContent += 
+`
+        p(style="margin-left:5px;font-size:25px") ` + category.title + "\n";
             category.samples.forEach(sample => {
                 pugContent += 
-`                 li
-                    a(href='javascript:load("` + sample.PGID + `")') ` + sample.title + "\n";
+`
+            
+            div( id='`+ elemCountID +`', class="itemLine `+sample.title+` contentBlock", style="background-color: #ebebeb;height:120px;overflow: hidden;clear: both;margin: 5px;margin-bottom: 10px", onclick="GetPG('`+sample.PGID+`','`+elemCountID+`')")
+                img(src="`+ sample.icon +`",style="max-height:100%;max-width:120px;display: inline-block")
+                div(style="display: inline-block;width: calc(100% - 120px);vertical-align: top;padding: 5px;", class = "contentDiv`+ elemCountID +`" )
+                    h3(style="margin: 0px;font-size: 18px;margin-bottom: 5px",class="itemLineChild") `+ sample.title +`
+                    p(style="margin: 0px;font-size: 15px;margin-bottom: 3px",class="itemLineChild") `+ sample.description +`
+                    a(href="`+ sample.doc +`", style="font-size: 15px;text-decoration:underline;text-decoration-color='#000000';color='#000000'",class="itemLineChildLink childLink",) >Documentation `;
+                    
+                    elemCountID++;
             });
         });
 
         pugContent += 
-`        div(style="width:calc(100% - 200px);float:right; display:inline-block")
-             iframe(id='iframe', width='100%', scrolling='no', frameborder='no', height='100%')
+`    
+    div(style="width:calc(100% - 400px);height:100%;float:right;display:inline-block", id='parentIframe')
+        iframe(id='iframeExample', name='iframeExample', src='http://playground.babylonjs.com/frame.html',style="width:100%;height:100%")
 include includes/footer.pug
 script(src='/js/examples.js')
 `;
 
         var htmlRender = pug.render(pugContent, { filename:"views/examples.pug", pretty: false, currentUrl: '/' });
-        fs.writeFileSync('public/html/examples.html', htmlRender);
+        fs.writeFileSync('public/html/examples/index.html', htmlRender);
         logger.info("> examples.html compiled.");
         if (done) {
             done();
