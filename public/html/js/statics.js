b0aea0589e03da102ac1af6cfcd4b19704161445
Trevor Baron <trbaron@microsoft.com>
2018-05-11 15:46:05 -0700

    add search bar to guides pages

--- public/html/js/statics.js
+++ public/html/js/statics.js
@@ -31,4 +31,35 @@
         });
         return false;
     }
+
+    var filterBar = document.querySelector("#filterBar");
+    var filter = function() {
+        let value = filterBar.value.toLowerCase();
+        var lines = document.getElementsByClassName("checkLocal");
+
+        for (var lineIndex = 0; lineIndex < lines.length; lineIndex++) {
+            var line = lines[lineIndex];
+            if ((line.innerText.toLowerCase()+line.children[0].children[0].innerText.toLowerCase()).indexOf(value) > -1) {
+                line.parentElement.style.display = "";
+            } else {
+                line.parentElement.style.display = "none";
+            }
+        }
+
+        var categories = document.getElementsByClassName("categoryContainer");
+        var displayCount = categories.length;
+
+        for(var categoryIndex = 0; categoryIndex < categories.length; categoryIndex++) {
+            var category = categories[categoryIndex];
+            category.style.display = "block";
+            if(category.clientHeight < 25) {
+                category.style.display = "none";
+                displayCount --;
+            }
+        }
+
+        if(displayCount == 0) document.getElementById("noResultsContainer").style.display = "block";
+        else  document.getElementById("noResultsContainer").style.display = "none";
+    }
+    filterBar.addEventListener('input', filter);
 })(jQuery);
